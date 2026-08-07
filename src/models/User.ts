5563d2687e69b8dd6cb3229559dd88a0c534969e
JSanchezs024 <joaquin.sanchez.suazo@gmail.com>
2026-08-06 23:05:58 -0400

    avance V2.1

---
+++ src/models/User.ts
@@ -0,0 +1,34 @@
+import mongoose, {Schema, Document} from "mongoose"
+
+export interface IUSer extends Document {
+    email: string
+    password: string
+    name: string
+    confirmed: boolean
+}
+
+const userSchema: Schema = new Schema({
+    email : {
+        type: String,
+        required: true,
+        lowercase: true,
+        unique: true
+    },
+    password:{
+        type: String,
+        requierd: true
+    },
+    name:{
+        type: String,
+        requierd: true
+    },
+    confirm:{
+        type: Boolean,
+        default: false
+    },
+})
+
+const User = mongoose.model<IUSer>('User', userSchema)
+export default User
+
+
