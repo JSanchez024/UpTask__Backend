95937c54fd95b6bebbe23ec0b464ae69d8f5ae3d
JSanchezs024 <joaquin.sanchez.suazo@gmail.com>
2026-08-30 13:17:17 -0400

    avance v3.5

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -3,6 +3,8 @@ import { CorsOptions } from "cors"
 export const corsConfig: CorsOptions = {
     origin: function(origin, callback){
 
+        const frontendUrl = process.env.FRONTEND_URL ? process.env.FRONTEND_URL.replace(/\/$/, '') : '';
+
         const whitelist = [process.env.FRONTEND_URL]
         if(process.argv[2] === '--api'){
             whitelist.push(undefined)
@@ -10,6 +12,7 @@ export const corsConfig: CorsOptions = {
         if(whitelist.includes(origin)){
             callback(null, true)
         }else{
+            console.error(`[CORS Error] Origen bloqueado: ${origin}`);
             callback(new Error('Error de CORS'))
         }
     }
